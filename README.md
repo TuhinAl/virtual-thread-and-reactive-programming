32dae3aefeef160bc9f55e5bc09a143572579aec
Tuhin <md.alauddin@bjitgroup.com>
2024-04-29 17:32:10 +0600

    platform thread creation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # virtual-thread-and-reactive-programming
+
+Process: 
+=======
+A process is an instance of a computer program which has its own isolated memory space. (unit of resources)
+
+Thread: 
+======
+A Thread is a unit of program or part of process that share the same memory space.(unit of execution)
+
+
+# I/O using thread.sleep()
+
+# virtual threads are daemon threads by default.
+
+# Virtual Thread
+Virtual Thread is simply an illusion provided by java
+It will look like Thread, It will accept Runnable, we can do thread.start(),
+ thread.sleep() 
+### But OS cannot see them/Schedule them.
+
+## Virtual Thread ==> Task
--- src/main/java/com/altuhin/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/com/altuhin/sec01/InboundOutboundTaskDemo.java
@@ -1,18 +1,61 @@
 package com.altuhin.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
 
-    private static final int MAX_PLATFORM = 50_0000;
+    private static final int MAX_PLATFORM = 10;
+    private static final int MAX_VIRTUAL = 10;
 
-    public static void main(String[] args) {
-        platformThreadDemo();
+    public static void main(String[] args) throws InterruptedException {
+        virtualThreadDemo();
     }
 
-    private static void platformThreadDemo() {
+    private static void platformThreadDemo1() {
         for (int i = 0; i < MAX_PLATFORM; i++) {
             int j = i;
             Thread thread = new Thread(() -> Task.ioIntensive(j));
             thread.start();
         }
+    }
+
+    private static void platformThreadDemo2() {
+        Thread.Builder builder = Thread.ofPlatform().name("Thread");
+        for (int i = 0; i < MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> Task.ioIntensive(j));
+            thread.start();
+        }
+    }
+
+    private static void platformThreadDemo3() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(MAX_PLATFORM);
+        Thread.Builder builder = Thread.ofPlatform().daemon().name("Daemon", 1);
+        for (int i = 0; i < MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
+
+    }
+
+
+    private static void virtualThreadDemo() throws InterruptedException {
+        Thread.Builder builder = Thread.ofVirtual().name("Virtual-", 1);
+        CountDownLatch latch = new CountDownLatch(MAX_VIRTUAL);
+        for (int i = 0; i < MAX_VIRTUAL; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
+
     }
 }
